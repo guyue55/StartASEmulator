7449a444a0161446311dc324ca2703fd38241bc1
caopeiya <peiya.cao@schinper.com>
2024-07-25 18:14:56 +0800

    初始化目录结构

---
+++ start_as_emulator/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+"""
+--------------------------------
+Name:     ___init___.py
+Author:   cpy
+Date:     2024/07/25
+Company:  古月居
+Description: 
+--------------------------------
+"""
+
+# ''' Standard library '''
+# from typing import List, Dict, Tuple, Any
+
+# ''' Third party Library '''
+
+# ''' Custom Library '''
---
+++ start_as_emulator/main.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+"""
+--------------------------------
+Name:     xxx.py
+Author:   cpy
+Date:     2024/07/25
+Company:  古月居
+Description: 
+--------------------------------
+"""
+
+# ''' Standard library '''
+# from typing import List, Dict, Tuple, Any
+
+# ''' Third party Library '''
+
+# ''' Custom Library '''
